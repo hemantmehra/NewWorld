5e01ee124870ed2d7b9d7f41e6543b63bf0bfb4a
Hemant Mehra <mehra.hemant14@gmail.com>
2024-12-30 19:03:34 +0530

    add blink title

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <assert.h>
+#include <iostream>
 #include <raylib.h>
 #include "city.h"
 #include "unit.h"
@@ -7,9 +8,56 @@
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 
+enum UI_Type {
+    UI_Map,
+    UI_City
+};
+
+void draw_map(int rows, int cols, int map_title_size, Texture2D grass_texture, Texture2D water_texture, int map[50][50])
+{
+    for (int i=0; i<rows; i++) {
+        for (int j=0; j<cols; j++) {
+            if (map[i][j] == 1) {
+                DrawTexture(grass_texture, j*map_title_size, i*map_title_size, WHITE);
+            }
+            else if (map[i][j] == 0) {
+                DrawTexture(water_texture, j*map_title_size, i*map_title_size, WHITE);
+            }
+        }
+    }
+}
+
+void draw_cities(std::vector<City*> *city_vec, Texture2D city_texture, int map_title_size)
+{
+    for (auto city : *city_vec) {
+        DrawTexture(city_texture, city->m_y*map_title_size, city->m_x*map_title_size, WHITE);
+    }
+}
+
+void draw_units(std::vector<Unit*> *unit_vec, Texture2D settler_texture, int map_title_size)
+{
+    for (auto unit : *unit_vec) {
+        if (unit->is_alive()) {
+            switch(unit->type()) {
+                case U_Settler:
+                    DrawTexture(
+                        settler_texture,
+                        unit->m_y*map_title_size,
+                        unit->m_x*map_title_size,
+                        WHITE
+                    );
+                    break;
+                default:
+                    assert(false);
+            }
+        }
+    }
+}
+
 int main()
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "CIV");
+    SetTargetFPS(30);
     Texture2D grass_texture = LoadTexture("assets/Grass.png");
     Texture2D water_texture= LoadTexture("assets/Water.png");
     Texture2D city_texture = LoadTexture("assets/City.png");
@@ -17,6 +65,8 @@ int main()
 
     std::vector<Unit*> unit_vec;
     std::vector<City*> city_vec;
+    City *current_city;
+    UI_Type current_ui = UI_Map;
 
     Unit *settler_unit = new Unit(U_Settler, 10, 10);
 
@@ -33,6 +83,10 @@ int main()
         }
     }
 
+    int frame_counter = 0;
+    const int frameDelay = 10;
+    bool visible = true;
+
     while(!WindowShouldClose()) {
         if (IsKeyReleased(KEY_RIGHT)) settler_unit->m_y++;
         if (IsKeyReleased(KEY_LEFT)) settler_unit->m_y--;
@@ -43,40 +97,48 @@ int main()
             settler_unit->consume();
         }
 
-        BeginDrawing();
-            ClearBackground(BLACK);
-            for (int i=0; i<rows; i++) {
-                for (int j=0; j<cols; j++) {
-                    if (map[i][j] == 1) {
-                        DrawTexture(grass_texture, j*map_title_size, i*map_title_size, WHITE);
-                    }
-                    else if (map[i][j] == 0) {
-                        DrawTexture(water_texture, j*map_title_size, i*map_title_size, WHITE);
-                    }
-                }
-            }
+        if (IsKeyReleased(KEY_Q)) {
+            current_ui = UI_Map;
+        }
+
+        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+            int mx = GetMouseX() / map_title_size;
+            int my = GetMouseY() / map_title_size;
 
             for (auto city : city_vec) {
-                DrawTexture(city_texture, city->m_y*map_title_size, city->m_x*map_title_size, WHITE);
-            }
-
-            for (auto unit : unit_vec) {
-                if (unit->is_alive()) {
-                    switch(unit->type()) {
-                        case U_Settler:
-                            DrawTexture(
-                                settler_texture,
-                                unit->m_y*map_title_size,
-                                unit->m_x*map_title_size,
-                                WHITE
-                            );
-                            break;
-                        default:
-                            assert(false);
-                    }
+                if (city->m_x == mx && city->m_y == my) {
+                    current_ui = UI_City;
+                    current_city = city;
+                    break;
                 }
             }
+        }
 
+        frame_counter++;
+        if (frame_counter >= frameDelay) {
+            frame_counter = 0;
+            visible = !visible;
+        }
+
+        BeginDrawing();
+            ClearBackground(BLACK);
+
+            switch(current_ui) {
+                case UI_Map: {
+                    draw_map(rows, cols, map_title_size, grass_texture, water_texture, map);
+                    draw_cities(&city_vec, city_texture, map_title_size);
+                    draw_units(&unit_vec, settler_texture, map_title_size);
+                }
+                break;
+
+                case UI_City: {
+                    DrawText("This is City UI", 0, 0, 24, WHITE);
+                }
+                break;
+            }
+
+            if (visible)
+                DrawTexture(water_texture, 5*map_title_size, 5*map_title_size, WHITE);
         EndDrawing();
     }
 
